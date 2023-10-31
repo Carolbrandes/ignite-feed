41adad10f4c540f222ac90358b871c29e8d9d063
carol <carolbrandes23@gmail.com>
2023-10-31 06:57:53 -0300

    Post

---
+++ src/components/Post/index.jsx
@@ -0,0 +1,39 @@
+import styles from "./style.module.css";
+
+export const Post = () => {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://avatars.githubusercontent.com/u/45435565?v=4"
+          />
+
+          <div className={styles.authorInfo}>
+            <strong>CarolB</strong>
+            <span>Front End Developer</span>
+          </div>
+        </div>
+
+        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">
+          publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋 </p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto #nlw #rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+};
